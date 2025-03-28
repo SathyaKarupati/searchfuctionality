0268cad5f17b73f7e3d8da4ef716226bed694cd0
SathyaKarupati <chinnukarupati@gmail.com>
2025-03-28 22:48:00 +0530

    Update Moviecard.tsx

--- src/component/Moviecard.tsx
+++ src/component/Moviecard.tsx
@@ -50,10 +50,10 @@ const Moviecard: React.FC<MovieCardProps> = ({ movie }) => {
     </Typography> 
     <Typography
           variant="body2"
-          color={movie.vote_average ? "primary" : "text.secondary"}
+          color={movie.Vote_average ? "primary" : "text.secondary"}
           sx={{ fontWeight: "medium" }}
         >
-          Rating: {movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"}{" "}
+          Rating: {movie.Vote_average ? movie.Vote_average.toFixed(1) : "N/A"}{" "}
           / 10
         </Typography>
 </CardContent> 
